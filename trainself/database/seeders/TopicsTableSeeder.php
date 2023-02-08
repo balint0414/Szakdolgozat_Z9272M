2b7a60fc8c53d80e55c848a0a94b4f3db6db426d
Bálint <g.balint0414@gmail.com>
2023-02-08 15:33:10 +0100

    Topics és Posts tábla megvalósítás adatbázis

---
+++ trainself/database/seeders/TopicsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Topic;
+
+class TopicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Topic::create(['title'=>'Edzés cikkek', 'description'=>'Cikkek az edzéssel kapcsolatban']);
+        Topic::create(['title'=>'Táplálkozás', 'description'=>'Cikkek a helyes táplálkozással kapcsolatban']);
+        Topic::create(['title'=>'Edzők', 'description'=>'Az oldalon elérhető edzők listája']);
+        Topic::create(['title'=>'Edzőtermek', 'description'=>'Az oldalon elérhető edzőtermek listája']);
+    }
+}
